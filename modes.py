e15d53d29094597359e0c50243a05d8f0f6bd6e0
JanBaumy <jan1207@hotmail.de>
2023-04-04 09:35:10 +0200

    added the same functionality without temperature

--- modes.py
+++ modes.py
@@ -4,13 +4,14 @@ from dataAnalysis import *
 from saveData import *
 import time
 
-#function to start the temperature measurement in parallel
-def parallel_temperature(queue):
-    queue.put(measure_temperature())
-
+#function to apply a single voltage and return the current
 def single_voltage(voltage):
     fug_set_voltage(voltage)
-    time.sleep(0.05) #wait 50ms for the voltage to stabilize
+
+    #wait for valid current measurement
+    while not valid_current():
+        pass
+
     current = measure_current()
     return current
 
@@ -19,12 +20,17 @@ def voltage_sweep(start_voltage, end_voltage, step):
     #end voltage may never be exceeded
     while (start_voltage + step) <= end_voltage:
         fug_set_voltage(start_voltage)
-        time.sleep(0.05) #wait 50ms for the voltage to stabilize
+
+        #wait for valid current measurement
+        while not valid_current():
+            pass
+
         current = measure_current()
         start_voltage += step
         yield start_voltage, current
 
 #function to go through each temperature
+#config needs to be a dict with these keys: mode, limit_resistor AND EITHER voltage OR start_voltage, end_voltage, step
 def temperature_sweep(temperature_list, temperature_tolerance, config):
     #check for validity of config dict
     if 'mode' not in config:
@@ -65,6 +71,8 @@ def temperature_sweep(temperature_list, temperature_tolerance, config):
             actual_temperature = round(measure_temperature(), 3)
             dut_voltage = calculateDUTVoltage(set_voltage=voltage, measured_current=current, limit_resistor=limit_resistor)
             current = single_voltage(voltage)
+
+            #make a line to save to csv
             csv_line = [set_temperature, actual_temperature, dut_voltage, current]
             if save_to_file:
                 append_to_csv(file_path, csv_line)
@@ -75,12 +83,66 @@ def temperature_sweep(temperature_list, temperature_tolerance, config):
             end_voltage = config.get('end_voltage')
             step = config.get('step')
 
+            #generator function returns the applied voltage and the measured current
             for voltage, current in voltage_sweep(start_voltage, end_voltage, step):
                 actual_temperature = round(measure_temperature(), 3)
                 dut_voltage = calculateDUTVoltage(set_voltage=voltage, measured_current=current, limit_resistor=limit_resistor)
+
+                #make a line to save to csv
                 csv_line = [set_temperature, actual_temperature, dut_voltage, current]
                 if save_to_file:
                     append_to_csv(file_path, csv_line)
+
+    return True
+
+#function to do everything without temperature
+#config needs to be a dict with these keys: mode, limit_resistor AND EITHER voltage OR start_voltage, end_voltage, step
+def no_temperature(config):
+    #check for validity of config dict
+    if 'mode' not in config:
+        print('ERROR: No mode selected')
+        return False
+    
+    mode = config.get('mode')
+    if mode != 'voltage' and mode != 'voltage_sweep':
+        print('ERROR: Invalid mode selected')
+        return False
+    
+    #extract information for all modes
+    limit_resistor=config.get('limit_resistor')
+    if 'file_path' in config:
+        file_path = config.get('file_path')
+        save_to_file = True
+    else:
+        save_to_file = False
+
+    #start the corresponding measurement
+    if mode == 'voltage':
+        #get parameters from input dict
+        voltage = config.get('voltage')
+        
+        current = single_voltage(voltage)
+        dut_voltage = calculateDUTVoltage(set_voltage=voltage, measured_current=current, limit_resistor=limit_resistor)
+
+        #make a line to save to csv
+        csv_line = [dut_voltage, current]
+        if save_to_file:
+            append_to_csv(file_path, csv_line)
+
+    elif mode == 'voltage_sweep':
+        #get parameters from input dict
+        start_voltage = config.get('start_voltage')
+        end_voltage = config.get('end_voltage')
+        step = config.get('step')
+
+        #generator function returns the applied voltage and the measured current
+        for voltage, current in voltage_sweep(start_voltage, end_voltage, step):
+            dut_voltage = calculateDUTVoltage(set_voltage=voltage, measured_current=current, limit_resistor=limit_resistor)
+
+            #make a line to save to csv
+            csv_line = [dut_voltage, current]
+            if save_to_file:
+                append_to_csv(file_path, csv_line)
 
     return True
 
