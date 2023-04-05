e7ac9e4367567352199b724e6cc580fc7710ab59
JanBaumy <jan1207@hotmail.de>
2023-04-05 07:26:33 +0200

    changed to a fake test

--- Tests/controlTestResistor.py
+++ Tests/controlTestResistor.py
@@ -2,11 +2,13 @@
 from sys import path
 path.append('../Curvetracer Control Software')
 from configLoader import import_config, check_config
+from saveData import check_and_create_file
 from modes import no_temperature, temperature_sweep
+from fakeTestFunctions import fake_no_temperature, fake_temperature_sweep
 
 def main():
     #get config
-    config_path = r'Basic Configs\invalid_config.json'
+    config_path = r'Basic Configs\1M2_resistor_test_with_temp.json'
     config = import_config(config_path)
     #check config
     valid_config = check_config(config)
@@ -20,16 +22,15 @@ def main():
     print('-----------------')
     file_path = input('INPUT: Enter the name of the file to save the data to (with .csv): ')
     config['file_path'] = config.get('save_folder') + f'\{file_path}'
+    check_and_create_file(config.get('file_path'), has_temperature = config.get('has_temperature'))
     print(f'INFO: File will be saved to: {config.get("file_path")}')
 
     print("INFO: Measurement starting----------------")
     #start measurement
     if config.get('has_temperature') == True:
-        #temperature_sweep(config)
-        pass
+        fake_temperature_sweep(config)
     else:
-        #no_temperature(config)
-        pass
+        fake_no_temperature(config)
 
 if __name__ == '__main__':
     main()
