4e3a0c8844a9d4ec7cbfc89d506dfa9dc0ce5f84
JanBaumy <jan1207@hotmail.de>
2023-04-06 10:37:06 +0200

    no longer plot previous data

--- plotData.py
+++ plotData.py
@@ -9,9 +9,9 @@ from configLoader import import_config
 
 def draw_plots(config):
     #read file (returns empty data when no file exists)
-    def read_file(file_path):
+    def get_raw_data(file_path, previous_lines):
         if os.path.exists(file_path):
-            data = np.genfromtxt(file_path, delimiter=';', skip_header=1)
+            data = np.genfromtxt(file_path, delimiter=';', skip_header=previous_lines)
             return data
         else:
             print("WARNING: No data file found")
@@ -53,8 +53,8 @@ def draw_plots(config):
             return (range(len(voltage)), (voltage, current))
     
     #update function needs the axs object and data in the format (voltage, current (,temperature))
-    def update(i, axs):
-        data = read_file(file_path)
+    def update(i, axs, previous_lines):
+        data = get_raw_data(file_path, previous_lines)
 
         #only plot when there is data
         if data.size > 0:
@@ -67,28 +67,50 @@ def draw_plots(config):
             for ax in axs:
                 ax.plot(animation_data[plot_nr][0][:i], animation_data[plot_nr][1][:i], color=colors[plot_nr])
                 plot_nr += 1
-    
+
+    #style the plot 
+    def init_plot(has_temperature):
+        plt.style.use('dark_background')
+        gs = GridSpec(nrows=2, ncols=2)
+
+        if has_temperature:
+            fig, axs = plt.subplots(3, 1)
+            axs[0].set_subplotspec(gs[0, 0])
+
+            axs[1].set_subplotspec(gs[:, 1])
+
+            axs[2].set_subplotspec(gs[1, 0])
+            axs[2].set_title('Temperature vs Time')
+            axs[2].set_xlabel('Measurement step')
+            axs[2].set_ylabel('Temperature [°C]')
+
+        else:
+            fig, axs = plt.subplots(2, 1)
+            axs[0].set_subplotspec(gs[:, 0])
+
+            axs[1].set_subplotspec(gs[:, 1])
+
+        #the same whether temperature is plotted or not
+        axs[0].set_title('Voltage vs Time')
+        axs[0].set_xlabel('Measurement step')
+        axs[0].set_ylabel('Voltage [V]')
+
+        axs[1].set_title('Current vs Voltage')
+        axs[1].set_xlabel('Voltage [V]')
+        axs[1].set_ylabel('Current [A]')
+
+        return fig, axs
+
     has_temperature = True if config.get('has_temperature') == True else False
     file_path = config.get('file_path')
 
-    plt.style.use('dark_background')
-    gs = GridSpec(nrows=2, ncols=2)
-
-    if has_temperature:
-        fig, axs = plt.subplots(3, 1)
-        axs[0].set_title('Voltage vs Time')
-        axs[0].set_subplotspec(gs[0, 0])
-        axs[1].set_title('Current vs Voltage')
-        axs[1].set_subplotspec(gs[:, 1])
-        axs[2].set_title('Temperature vs Time')
-        axs[2].set_subplotspec(gs[1, 0])
+    #in order not to plot previous data in file
+    if os.path.exists(file_path):
+        previous_lines = sum(1 for line in open(file_path))
     else:
-        fig, axs = plt.subplots(2, 1)
-        axs[0].set_title('Voltage vs Time')
-        axs[0].set_subplotspec(gs[:, 0])
-        axs[1].set_title('Current vs Voltage')
-        axs[1].set_subplotspec(gs[:, 1])
+        previous_lines = 1
 
+    fig, axs = init_plot(has_temperature)
 
     #stylize the plots
     fig.suptitle('Real-Time Data')
@@ -100,9 +122,9 @@ def draw_plots(config):
         ax.grid(color='white', linestyle='-', linewidth=0.2)
 
     #plotting at an interval of 10ms
-    anim = FuncAnimation(fig, update, fargs=(axs,), cache_frame_data=False, interval=10)
+    anim = FuncAnimation(fig, update, fargs=(axs, previous_lines), cache_frame_data=False, interval=10)
  
-    return anim
+    return fig, anim
 
 if __name__ == '__main__':
     config_path = r'Basic Configs\1M2_resistor_test_with_temp.json'
