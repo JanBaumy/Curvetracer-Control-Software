03a78ad53fc6306af0c9edf41cd0f18e0c379785
JanBaumy <jan1207@hotmail.de>
2023-04-06 07:11:11 +0200

    added 'maximum_current' and ability to modify conf

--- configMaster.py
+++ configMaster.py
@@ -6,8 +6,9 @@ measurement_folder = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desk
 
 no_temp_single_voltage = dict(has_temperature=False,\
                                 mode='voltage',\
-                                limit_resistor='short',\
+                                limit_resistor='12M',\
                                 input_current=5e-3,\
+                                maximum_current=50e-6,\
                                 voltage=3000,\
                                 save_to_file=True,\
                                 save_folder=measurement_folder)
@@ -16,6 +17,7 @@ no_temp_sweep_voltage = dict(has_temperature=False,\
                                 mode='voltage_sweep',\
                                 limit_resistor='12M',\
                                 input_current=5e-3,\
+                                maximum_current=50e-6,\
                                 start_voltage=0,\
                                 end_voltage=3000,\
                                 step=50,\
@@ -26,6 +28,7 @@ temp_single_voltage = dict(has_temperature=True,\
                                 mode='voltage',\
                                 limit_resistor='12M',\
                                 input_current=5e-3,\
+                                maximum_current=50e-6,\
                                 temperature_list=[22, 32, 42, 52, 62, 72, 82, 92, 102],\
                                 temperature_tolerance=2,\
                                 save_to_file=True,\
@@ -35,6 +38,7 @@ temp_sweep_voltage = dict(has_temperature=True,\
                                 mode='voltage_sweep',\
                                 limit_resistor='12M',\
                                 input_current=5e-3,\
+                                maximum_current=50e-6,\
                                 temperature_list=[22, 32, 42, 52, 62, 72, 82, 92, 102],\
                                 temperature_tolerance=2,\
                                 start_voltage=0,\
